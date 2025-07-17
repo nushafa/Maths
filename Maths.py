d8d96c3ed9d664029eb668970213bd52e843fcb5
Abiat Nushafa <nushafa1@gmail.com>
2025-07-17 20:59:59 +0600

    Add files via upload

---
+++ Maths.py
@@ -0,0 +1,6 @@
+import math
+print(math.isclose(12.014, 12.56))
+print(math.isclose(12.014, 12.014))
+print(math.isclose(12.4, 12.46))
+print(math.isclose(12.014, 12.434, abs_tol = 0.5))
+print(math.isclose(12.014, 12.018, rel_tol = 0.02))
